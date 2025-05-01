6e9183eac4a4eab68d285bc8109cc20bdcce6bb3
khushal kumar <khushalsuthar546@gmail.com>
2025-05-01 18:46:57 +0530

    Initial commit with Java series files

--- new practice/ConstructorDefault.java
+++
@@ -1,9 +0,0 @@
-class ConstructorDefault{
-ConstructorDefault(){
-    System.out.println("Constructor Default Created");
-    }
-
-public static void main(String args[]){ 
-ConstructorDefault b=new ConstructorDefault();
-} 
-}
